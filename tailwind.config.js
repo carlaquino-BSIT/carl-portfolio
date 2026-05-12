c16dbef1bd504e3bdd5b1162bb79d61d9fbcad8f
carlaquino-BSIT <carlangeloaquino2502@gmail.com>
2026-05-12 23:13:10 +0800

    Refactor fontFamily configuration in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,35 @@
+// export default {
+//   darkMode: 'class',
+//   content: ['./index.html', './src/**/*.{vue,js,ts}'],
+
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         mono: [
+//           'ui-monospace',
+//           'SFMono-Regular',
+//           'Menlo',
+//           'Monaco',
+//           'Consolas',
+//           'monospace'
+//         ]
+//       }
+//     },
+//   },
+
+//   plugins: [],
+// }
+
 export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
-
   theme: {
     extend: {
       fontFamily: {
-        mono: [
-          'ui-monospace',
-          'SFMono-Regular',
-          'Menlo',
-          'Monaco',
-          'Consolas',
-          'monospace'
-        ]
-      }
+        sans: ['Inter', 'sans-serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'monospace'],
+      },
     },
   },
-
   plugins: [],
 }
